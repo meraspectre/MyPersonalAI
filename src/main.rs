38554915bd8d31b924b61b9a55b9692ae9c49e68
meraspectre <voltaireblack@gmail.com>
2024-09-28 19:36:11 +0300

    ya razduplisya s experimentom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{} Guys, {} Born {}", repository_qq(3, 4, 5), repository_qq(555, 666, 777), repository_qq(1488, 4228, 1337));
+}
+fn repository_qq(a:i32,b:i32,c:i32) -> i32 {
+    let g = a + b + c;
+    g
+    
 }
